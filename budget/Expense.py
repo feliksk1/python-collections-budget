12c7bc3feed0e7845afbe13fa8969e28bdee3e80
feliksk <feliksk@gmail.com>
2021-05-23 11:09:27 -0700

    module 4

--- budget/Expense.py
+++ budget/Expense.py
@@ -40,3 +40,13 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+        
+    def categorize_set_comprehension(self): 
+        necessary_categories = {'Phone', 'Auto and Gas', 'Classes', 'Utilities', 'Mortgage'}
+        necessary_expenses = {x for x in self.list if x.category in necessary_categories}
+        food_expenses = {x for x in self.list if x.category in {'Groceries', 'Eating Out'}}
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
+                              #||  x.category == 'Auto and Gas' ||          x.category == 'Classes' ||  x.category == 'Utilities' || x.category == 'Mortgage'}
+
+        
