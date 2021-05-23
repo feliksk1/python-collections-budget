cb72fc90237537d24061e28613c22d063fc74065
feliksk <feliksk@gmail.com>
2021-05-23 11:18:13 -0700

    fixed tests

--- budget/Expense.py
+++ budget/Expense.py
@@ -42,11 +42,11 @@ class Expenses():
             return [necessary_expenses, food_expenses, unnecessary_expenses]
         
     def categorize_set_comprehension(self): 
-        necessary_categories = {'Phone', 'Auto and Gas', 'Classes', 'Utilities', 'Mortgage'}
-        necessary_expenses = {x for x in self.list if x.category in necessary_categories}
-        food_expenses = {x for x in self.list if x.category in {'Groceries', 'Eating Out'}}
+        necessary_expenses = {x for x in self.list 
+                              if x.category == 'Phone' or x.category == 'Auto and Gas' or x.category == 'Classes'  or x.category == 'Utilities' or x.category == 'Mortgage'}
+        food_expenses = {x for x in self.list 
+                         if x.category == 'Groceries' or x.category == 'Eating Out'}
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
         return [necessary_expenses, food_expenses, unnecessary_expenses]
-                              #||  x.category == 'Auto and Gas' ||          x.category == 'Classes' ||  x.category == 'Utilities' || x.category == 'Mortgage'}
 
         
